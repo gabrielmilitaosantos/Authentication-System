03f227fe538dcbdc88990677074de3a62a8fa834
Gabriel <gabrielmilitaosantos@gmail.com>
2025-07-22 17:20:15 -0300

    Create the function of insert users in db

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -17,6 +17,7 @@ class UserModel {
     try {
       await sql`
          CREATE TABLE IF NOT EXISTS users (
+         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
          name TEXT NOT NULL,
          email TEXT NOT NULL UNIQUE,
          password TEXT NOT NULL,
@@ -24,7 +25,7 @@ class UserModel {
          verifyOtpExpires INT DEFAULT 0,
          isAccountVerified BOOLEAN DEFAULT false,
          resetOtp TEXT DEFAULT NULL,
-         resetOtpExpires INT DEFAULT 0,
+         resetOtpExpires INT DEFAULT 0
          )         
          `;
       console.log("Users table created or already exists.");
@@ -32,6 +33,39 @@ class UserModel {
       console.error("Error creating users table:", error);
       throw error;
     }
+  }
+
+  async createUser(name, email, password) {
+    try {
+      const [user] = await sql`
+        INSERT INTO users (name, email, password)
+        VALUES (${name}, ${email}, ${password})
+        RETURNING id, name, email
+      `;
+
+      return user;
+    } catch (error) {
+      console.error("Error creating user:", error);
+      throw error;
+    }
+  }
+
+  async emailExists(email) {
+    try {
+      const [user] = await sql`
+        SELECT id FROM users WHERE email = ${email}
+      `;
+
+      return !!user; // Return true if user exists, false otherwise
+    } catch (error) {
+      console.error("Error checking if email exists:", error);
+      throw error;
+    }
+  }
+
+  // close connections (use this when shutting down the server)
+  async close() {
+    await sql.end();
   }
 }
 
